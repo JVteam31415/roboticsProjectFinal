a7c7beffca632d0d58d8c386b1a2aa5eb7a8b208
James VanderKam <jamesvanderkam@verizon.net>
2021-11-01 14:47:52 -0400

    now with a fwdkin function that works on dobot 3

--- fwdkinArduino.m
+++ fwdkinArduino.m
@@ -1,12 +1,14 @@
-clear all;
-serialportlist("available")'
-arduinoObj = serialport("COM6",9600)
+function robot= fwdkinArduino(robot,q,arduinoObj )
+
 configureTerminator(arduinoObj,hex2dec('5A')); % Data package ends with byte 0x5A
+
 low_lim = 10;
 up_lim = 40;
 N = 4; % Number of joints
 
-new_angles = low_lim + (up_lim-low_lim) .* rand(N,1)
+
+new_angles = q
+
 isGrab = 0; % No suction
 
 MOVE_MODE_JUMP = 0;
@@ -32,15 +34,18 @@ line_chr_cmd = char(typecast(single(line_float_cmd), 'uint8'));
 % tail_chr = char(hex2dec('5A')); % No need, bcs already specified with configureTerminator command
 line_chr_cmd = [header_chr,line_chr_cmd]; %,tail_chr]
 line_str_cmd = convertCharsToStrings(line_chr_cmd)
+
 % Send command to the robot
 writeline(arduinoObj,line_str_cmd)
 
 pause(1);
+
 flush(arduinoObj);
 line_str = readline(arduinoObj); % MATLAB returns readline as string
 line_str = readline(arduinoObj); % MATLAB returns readline as string
 line_chr = convertStringsToChars(line_str) % convert the string to char array
-line_hex = dec2hex(line_chr) % Just to see the data as byte values in hex
+
+% line_hex = dec2hex(line_chr) % Just to see the data as byte values in hex
 assert(line_chr(1) == char(hex2dec('A5'))) % Assertion for header byte
 assert(length(line_chr) == 41) % Assertion for data length is correct (ie 42 bytes, 1 is removed with end byte while reading the line)
 line_float = typecast(uint8(line_chr(2:end)), 'single') % obtain the 10 float(single) values in the data package
@@ -56,3 +61,6 @@ robot.angles.pawArmAngle = line_float(8); % Servo Angle (joint 4 angle)
 
 robot.isGrab = line_float(9) > 0; % Pump State
 robot.angles.gripperAngle = line_float(10); % Gripper Angle 
+
+
+end
